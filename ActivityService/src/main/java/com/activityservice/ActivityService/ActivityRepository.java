f626e60b1b41bf89c8290566d83ae32516d6ddba
manikandan-704 <nagarajpriyan2004@gmail.com>
2026-05-21 23:43:00 +0530

    ActivityService End points

--- ActivityService/src/main/java/com/activityservice/ActivityService/ActivityRepository.java
+++ ActivityService/src/main/java/com/activityservice/ActivityService/ActivityRepository.java
@@ -3,9 +3,13 @@ package com.activityservice.ActivityService;
 import com.activityservice.ActivityService.Model.Activity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
 
 @Repository
 public interface ActivityRepository extends MongoRepository<Activity, String> {
 
+    List<Activity> findByUserId(String userId);
 }
+
---
+++ ActivityService/src/main/java/com/activityservice/ActivityService/MongoConfig/Config.java
@@ -0,0 +1,10 @@
+package com.activityservice.ActivityService.MongoConfig;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class Config {
+}
